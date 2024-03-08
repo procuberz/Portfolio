2d55562ae7b3af47fd419d137e476a35045fce5b
procuberz <ruqiyanurmatova@gmail.com>
2024-03-08 19:23:17 +0500

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,47 @@
-import { Fragment } from "react";
-import AppStyle from "./App.module.scss";
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"
-import Services from "./pages/Services"
-import Portfolio from "./pages/Portfolio"
-import Contacts from "./pages/Contacts"
+import { Fragment } from 'react';
+import AppStyle from './App.module.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Contacts from './pages/Contacts';
 
 const App = () => {
   return (
     <Fragment>
-      <header className={AppStyle["header"]}>
-        <h1 className={AppStyle["title-1"]} >
+      <header className={AppStyle['header']}>
+        <h1 className={AppStyle['title-1']}>
           Nurmuhammad - фрилансер, веб-разработчик
         </h1>
 
-        <p className={AppStyle["subtitle"]}>
-          Всем привет я профессиональный фрилансер верстальщик.
-          Люблю создание сайтов и веб-разработку.
+        <p className={AppStyle['subtitle']}>
+          Всем привет я профессиональный фрилансер верстальщик. Люблю создание
+          сайтов и веб-разработку.
         </p>
 
-        <nav className={AppStyle["header-menu"]}>
-          <Link to={"/Portfolio"}>Главная</Link>
-          <Link to={"/Portfolio/Services"}>Услуги</Link>
-          <Link to={"/Portfolio/Portfolio"}>Портфолио</Link>
-          <Link to={"/Portfolio/Contacts"}>Контакты</Link>
+        <nav className={AppStyle['header-menu']}>
+          <Link to={'/portfolio'}>Главная</Link>
+          <Link to={'/portfolio/Services'}>Услуги</Link>
+          <Link to={'/portfolio/portfolio'}>Портфолио</Link>
+          <Link to={'/portfolio/Contacts'}>Контакты</Link>
         </nav>
       </header>
 
-      <main className={AppStyle["main"]}>
-
+      <main className={AppStyle['main']}>
         <Routes>
-          <Route path="/Portfolio" index element={<Home />} />
+          <Route path="/portfolio" index element={<Home />} />
 
-          <Route path="/Portfolio/Services" element={<Services />} />
+          <Route path="/portfolio/Services" element={<Services />} />
 
-          <Route path="/Portfolio/Portfolio" element={<Portfolio />} />
+          <Route path="/portfolio/portfolio" element={<Portfolio />} />
 
-          <Route path="/Portfolio/Contacts" element={<Contacts />} />
+          <Route path="/portfolio/Contacts" element={<Contacts />} />
         </Routes>
-
       </main>
 
-      <footer className={AppStyle["footer"]}>
-        &copy; 2024 Nurmuhammad
-      </footer>
-
+      <footer className={AppStyle['footer']}>&copy; 2024 Nurmuhammad</footer>
     </Fragment>
-  )
-}
+  );
+};
 
 export default App;
